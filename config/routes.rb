d1392006704dd677751800f7fe870dff871d8a7e
omensann <135625902+omensann@users.noreply.github.com>
2023-07-02 15:03:40 +0000

    [add]bookers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get '/books' => 'books#new'
-  post 'books' => 'books#create'
-  get 'books/index' => 'books#index'
-  get 'books/edit'
-  get 'books/show' => 'books#show'
   root to: 'homes#top'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+  #get '/books/new' => 'books#new'
+  #post '/books' => 'books#create'
+  #get '/books/index' => 'books#index'
+  #get '/books/:id/edit'  => 'books#edit'
+  #get '/books/:id' => 'books#show', as: 'book'
+  #delete '/books/:id' => 'books#destroy', as: 'destroy_book'
+#, only:[:index, :show, :new, :edit, :update, :destroy, :create]
